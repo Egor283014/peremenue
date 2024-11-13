83f3c2a381c2aa5e98377323b8ca82c8b8cd58e5
Egor283014 <283014egor@gmail.com>
2024-11-13 04:13:27 +0300

    Add files via upload

---
+++ peremenue.py
@@ -0,0 +1,21 @@
+The_number_of_completed_DZ = 12
+print(The_number_of_completed_DZ)
+#Количество выполненных ДЗ (запишите значение 12)
+Number_of_hours_spent = 1.5
+print(Number_of_hours_spent)
+#Количество затраченных часов (запишите значение 1.5)
+name_course = 'Python'
+#Название курса (запишите значение 'Python')
+print(name_course)
+#Время на одно задание (вычислить используя 1 и 2 переменные)
+Time_for_one_task = Number_of_hours_spent / The_number_of_completed_DZ
+print(Time_for_one_task)
+
+#либо чтобы упростить код
+tasks = 12
+print(tasks)
+time_spent = 1.5
+print(time_spent)
+name_course = 'python'
+print(name_course)
+execution_time = time_spent /  tasks
